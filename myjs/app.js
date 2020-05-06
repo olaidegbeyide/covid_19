8a6e7e4adaf37fac49a38c101a65525a39658987
olaidegbeyide <toungbeyide9@gmail.com>
2020-05-06 03:46:40 +0100

    Adding some codes

--- myjs/app.js
+++ myjs/app.js
@@ -19,24 +19,26 @@ const loadStatistics = async () =>  {
 
 loadStatistics()
 
-const world_wide = document.querySelector('.table-worldwide')
+
+
+var _infected = document.querySelector('.infected')
+var _recovered = document.querySelector('.recovered')
+var _deceased = document.querySelector('.deceased')
+var arr = [];
+
 const loadStatistic = async () =>  {
-    let worldDetails = ``
     await fetch('https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true')
     .then(res => res.json())
     .then(data => {
-        data.map(_data => {
-        worldDetails += `
-        <tr>
-         
-          <td>${_data.infected}</td>
-          <td>${_data.recovered}</td>
-          <td>${_data.deceased}</td>
-        </tr>
-        `
-    })
-    world_wide.innerHTML = worldDetails
-  })
+  let infected_cases = data.filter(mydata => mydata.infected).map(mydata => mydata.infected && mydata.infected !== 'NA' ).reduce((a,b) => a + b);
+  let recovered_cases = data.filter(mydata => mydata.recovered).map(mydata => mydata.recovered && mydata.recovered !== 'NA' ).reduce((a,b) => a + b);
+  let deceased_cases = data.filter(mydata => mydata.deceased).map(mydata => mydata.deceased && mydata.deceased !== 'NA' ).reduce((a,b) => a + b);
+  
+  _infected.innerHTML = infected_cases;
+  _recovered.innerHTML = recovered_cases;
+  _deceased.innerHTML = deceased_cases;
+})
+
 }
 
 loadStatistic()
